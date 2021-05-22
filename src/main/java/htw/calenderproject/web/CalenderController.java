5d889f3d5ed2a81aa3e6d7bbce796d6ffa87e87e
Mariechenkäfer <s0576271@htw-berlin.de>
2021-05-22 13:58:49 +0200

    Controller

--- src/main/java/htw/calenderproject/web/CalenderController.java
+++ src/main/java/htw/calenderproject/web/CalenderController.java
@@ -1,9 +1,18 @@
 package htw.calenderproject.web;
 
 
+import htw.calenderproject.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalenderController {
+    @Autowired
+    EventRepository eventRepository;
 
+    @RequestMapping
+    public String homepage() {
+        return "Calender";
+    }
 }
