ae9eaed4d851cfb44e4f087763d2e2919507c736
Mariechenkäfer <s0576271@htw-berlin.de>
2021-05-22 13:54:29 +0200

    Event Repository

---
+++ src/main/java/htw/calenderproject/EventRepository.java
@@ -0,0 +1,14 @@
+package htw.calenderproject;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, Long> {
+    List<Event> findByUserName(String userName);
+    Event findById(long id);
+    Event findByDate(LocalDateTime date);
+}
