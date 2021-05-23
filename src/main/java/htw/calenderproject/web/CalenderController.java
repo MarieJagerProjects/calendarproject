784198eeb2ebe352df496c5134694afe93abe6fb
Mariechenkäfer <s0576271@htw-berlin.de>
2021-05-23 14:50:44 +0200

    okta

--- src/main/java/htw/calenderproject/web/CalenderController.java
+++ src/main/java/htw/calenderproject/web/CalenderController.java
@@ -5,8 +5,10 @@ import htw.calenderproject.EventRepository;
 import htw.calenderproject.config.Endpoints;
 import htw.calenderproject.config.ViewNames;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,8 +21,20 @@ public class CalenderController {
     public ModelAndView showIndexPage() {
         return new ModelAndView(ViewNames.INDEX);
     }
+
     @GetMapping(path = Endpoints.LOGIN)
-    public ModelAndView loginUser() {
+    public ModelAndView loginUser(@AuthenticationPrincipal OidcUser user, Model model) {
+        String email = user.getEmail();
+
+        model.addAttribute("email", email);
+        model.addAttribute("username", user.getGivenName());
+
         return new ModelAndView(ViewNames.LOGIN);
     }
+
+    @GetMapping(path = Endpoints.CALENDER)
+    public ModelAndView showCalender(){
+        return new ModelAndView(ViewNames.CALENDER);
+    }
+
 }
