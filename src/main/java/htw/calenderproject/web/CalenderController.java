84aa8c4dbef39f6ffe4e28ab45676efb07fead33
Mariechenkäfer <s0576271@htw-berlin.de>
2021-05-23 11:45:25 +0200

    GetMapping

--- src/main/java/htw/calenderproject/web/CalenderController.java
+++ src/main/java/htw/calenderproject/web/CalenderController.java
@@ -2,7 +2,10 @@ package htw.calenderproject.web;
 
 
 import htw.calenderproject.EventRepository;
+import htw.calenderproject.config.Endpoints;
+import htw.calenderproject.config.ViewNames;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +14,8 @@ public class CalenderController {
     @Autowired
     EventRepository eventRepository;
 
-    @RequestMapping
-    public String homepage() {
-        return "Calender";
+    @GetMapping (path = Endpoints.INDEX)
+    public String showIndexPage() {
+        return ViewNames.INDEX;
     }
 }
