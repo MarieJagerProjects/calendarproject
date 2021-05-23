248d38efb98cecf918ceb915265727b063ab1968
Mariechenkäfer <s0576271@htw-berlin.de>
2021-05-23 11:51:19 +0200

    ModelAndView in GetMapping

--- src/main/java/htw/calenderproject/web/CalenderController.java
+++ src/main/java/htw/calenderproject/web/CalenderController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class CalenderController {
@@ -15,7 +16,7 @@ public class CalenderController {
     EventRepository eventRepository;
 
     @GetMapping (path = Endpoints.INDEX)
-    public String showIndexPage() {
-        return ViewNames.INDEX;
+    public ModelAndView showIndexPage() {
+        return new ModelAndView(ViewNames.INDEX);
     }
 }
