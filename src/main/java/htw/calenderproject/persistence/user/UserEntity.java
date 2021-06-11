3ee6ffbf12e066707bff2b443d0dc3d937239dfe
Mariechenkäfer <s0576271@htw-berlin.de>
2021-06-11 11:45:57 +0200

    UserEntity und UserRepository

--- src/main/java/htw/calenderproject/persistence/user/UserEntity.java
+++ src/main/java/htw/calenderproject/persistence/user/UserEntity.java
@@ -1,4 +1,26 @@
 package htw.calenderproject.persistence.user;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+
 public class UserEntity {
-}
+    @Entity
+    @Table(name = "users")
+    public class UserEntity implements UserDetails {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        private long id;
+
+        @Column(name = "username", nullable = false, unique = true)
+        private String username;
+
+        @Column(name = "email")
+        private String email;
+
+        @Column(name = "encrypted_password", nullable = false, length = 60)
+        private String password;
+
+    }
--- src/main/java/htw/calenderproject/persistence/user/UserRepository.java
+++ src/main/java/htw/calenderproject/persistence/user/UserRepository.java
@@ -1,4 +1,11 @@
 package htw.calenderproject.persistence.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
