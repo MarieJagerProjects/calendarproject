324ce71c744b1170d47e2e9c1c32434d1933c77a
Mariechenkäfer <s0576271@htw-berlin.de>
2021-06-11 12:34:12 +0200

    user entity extends class

--- src/main/java/htw/calenderproject/persistence/user/UserEntity.java
+++ src/main/java/htw/calenderproject/persistence/user/UserEntity.java
@@ -1,14 +1,14 @@
 package htw.calenderproject.persistence.user;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
-public class UserEntity {
     @Entity
     @Table(name = "users")
     public class UserEntity implements UserDetails {
-
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "id")
@@ -21,6 +21,46 @@ public class UserEntity {
         private String email;
 
         @Column(name = "encrypted_password", nullable = false, length = 60)
-        private String password;
+        private String encryptedPassword;
 
+        protected UserEntity() {}
+
+        public UserEntity(String username, String email, String encryptedPassword) {
+            this.username = username;
+            this.email = email;
+            this.encryptedPassword = encryptedPassword;
+        }
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return null;
+        }
+
+        @Override
+        public String getPassword() {
+            return encryptedPassword;
+        }
+        @Override
+        public String getUsername() {
+            return username;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
     }
